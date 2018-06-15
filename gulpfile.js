1163abbebb712877b9baa086f0c1b0e27dcbd610
Radostin Emanuilov <emanuilov@techr.eu>
2018-06-15 17:27:25 +0300

    NW.JS changes

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,10 @@ var jasmine = require('gulp-jasmine-browser');
 var clean = require('gulp-clean');
 
 gulp.task('watch', function () {
-	gulp.watch('./src/sass/**/*.scss', gulp.parallel('styles'));
-	gulp.watch('./src/js/**/*.js', gulp.parallel('lint', 'scripts'));
-	gulp.watch('./src/img/*', gulp.parallel('minify-images'));
-	gulp.watch('./src/**/*.html', gulp.parallel('copy-html'));
+	gulp.watch('./src/sass/**/*.scss', gulp.series('styles', 'nw-reload-styles'));
+	gulp.watch('./src/js/**/*.js', gulp.parallel('lint', 'scripts', 'nw-reload-scripts'));
+	gulp.watch('./src/img/*', gulp.parallel('minify-images', 'nw-reload'));
+	gulp.watch('./src/**/*.html', gulp.series('copy-html', 'nw-reload'));
 });
 
 gulp.task('copy-html', function () {
@@ -79,6 +79,30 @@ gulp.task('clean', function () {
 	return gulp.src('./dist', {
 		read: false
 	}).pipe(clean());
+});
+
+gulp.task('nw-reload', function () {
+	if (location) location.reload();
+});
+
+gulp.task('nw-reload-styles', function () {
+	var styles = document.querySelectorAll('link[rel=stylesheet]');
+
+	for (var i = 0; i < styles.length; i++) {
+		// reload styles
+		var restyled = styles[i].getAttribute('href') + '?v=' + Math.random(0, 10000);
+		styles[i].setAttribute('href', restyled);
+	}
+});
+
+gulp.task('nw-reload-scripts', function () {
+	var scripts = document.querySelectorAll('script');
+
+	for (var i = 0; i < scripts.length; i++) {
+		// reload styles
+		var restyled = scripts[i].getAttribute('src') + '?v=' + Math.random(0, 10000);
+		scripts[i].setAttribute('src', restyled);
+	}
 });
 
 gulp.task('default', gulp.series('clean', gulp.parallel('copy-html', 'styles', 'lint', 'scripts', 'minify-images', 'watch')));
