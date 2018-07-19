901215c5114324e725dfdc15091ded8d4ac03dd2
Radostin Emanuilov <emanuilov@techr.eu>
2018-07-19 17:10:50 +0300

    ESlint changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,115 +1,115 @@
 /*eslint no-console: ["error", { allow: ["log","warn","error"] }] */
-const gulp = require('gulp'),
-	watch = require('gulp-watch'),
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	imagemin = require('gulp-imagemin'),
-	pngquant = require('imagemin-pngquant'),
-	eslint = require('gulp-eslint'),
-	babel = require('gulp-babel'),
-	concat = require('gulp-concat'),
-	sourcemaps = require('gulp-sourcemaps'),
-	uglify = require('gulp-uglify'),
-	jasmine = require('gulp-jasmine-browser'),
-	clean = require('gulp-clean'),
-	child_process = require('child_process');
+const gulp = require("gulp"),
+	watch = require("gulp-watch"),
+	sass = require("gulp-sass"),
+	autoprefixer = require("gulp-autoprefixer"),
+	imagemin = require("gulp-imagemin"),
+	pngquant = require("imagemin-pngquant"),
+	eslint = require("gulp-eslint"),
+	babel = require("gulp-babel"),
+	concat = require("gulp-concat"),
+	sourcemaps = require("gulp-sourcemaps"),
+	uglify = require("gulp-uglify"),
+	jasmine = require("gulp-jasmine-browser"),
+	clean = require("gulp-clean"),
+	child_process = require("child_process");
 let nwjs = false;
 
-gulp.task('watch', function () {
-	watch('./src/sass/**/*.scss', gulp.parallel('styles'));
-	watch('./src/js/**/*.js', gulp.series('js-lint', 'scripts'));
-	watch('./src/img/*', gulp.series('minify-images', 'restart-nwjs'));
-	watch('./src/**/*.html', gulp.series('copy-html', 'restart-nwjs'));
+gulp.task("watch", function () {
+	watch("./src/sass/**/*.scss", gulp.parallel("styles"));
+	watch("./src/js/**/*.js", gulp.series("js-lint", "scripts"));
+	watch("./src/img/*", gulp.series("minify-images", "restart-nwjs"));
+	watch("./src/**/*.html", gulp.series("copy-html", "restart-nwjs"));
 });
 
-gulp.task('copy-html', function () {
-	return gulp.src('./src/**/*.html')
-		.pipe(gulp.dest('./dist'));
+gulp.task("copy-html", function () {
+	return gulp.src("./src/**/*.html")
+		.pipe(gulp.dest("./dist"));
 });
 
-gulp.task('scripts', function () {
-	return gulp.src('./src/js/**/*.js')
+gulp.task("scripts", function () {
+	return gulp.src("./src/js/**/*.js")
 		.pipe(babel())
-		.pipe(concat('script.js'))
-		.pipe(gulp.dest('./dist/js'));
+		.pipe(concat("script.js"))
+		.pipe(gulp.dest("./dist/js"));
 });
 
-gulp.task('scripts-dist', function () {
-	return gulp.src(['./src/js/**/*.js', '!./src/js/dev-reload.js'])
+gulp.task("scripts-dist", function () {
+	return gulp.src(["./src/js/**/*.js", "!./src/js/dev-reload.js"])
 		.pipe(babel())
 		.pipe(sourcemaps.init())
-		.pipe(concat('script.js'))
+		.pipe(concat("script.js"))
 		.pipe(uglify())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./dist/js'));
+		.pipe(gulp.dest("./dist/js"));
 });
 
-gulp.task('js-lint', function () {
-	return gulp.src(['./src/js/**/*.js', '!node_modules/**'])
+gulp.task("js-lint", function () {
+	return gulp.src(["./src/js/**/*.js", "!node_modules/**", "!./src/js/jquery-3.3.1.js"])
 		.pipe(eslint())
 		.pipe(eslint.format())
 		.pipe(eslint.failAfterError());
 });
 
-gulp.task('js-tests', function () {
-	return gulp.src(['./tests/**/*.js'])
+gulp.task("js-tests", function () {
+	return gulp.src(["./tests/**/*.js"])
 		.pipe(jasmine.specRunner({
 			console: true
 		}))
 		.pipe(jasmine.headless({
-			driver: 'phantomjs'
+			driver: "phantomjs"
 		}));
 });
 
-gulp.task('styles', function () {
-	return gulp.src('./src/sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
+gulp.task("styles", function () {
+	return gulp.src("./src/sass/**/*.scss")
+		.pipe(sass().on("error", sass.logError))
 		.pipe(autoprefixer({
-			browsers: ['last 5 versions']
+			browsers: ["last 5 versions"]
 		}))
-		.pipe(gulp.dest('./dist/css'));
+		.pipe(gulp.dest("./dist/css"));
 });
 
-gulp.task('minify-images', function () {
-	return gulp.src('./src/img/*')
+gulp.task("minify-images", function () {
+	return gulp.src("./src/img/*")
 		.pipe(imagemin({
 			progressive: true,
 			use: [pngquant()]
 		}))
-		.pipe(gulp.dest('./dist/img'));
+		.pipe(gulp.dest("./dist/img"));
 });
 
-gulp.task('clean', function () {
-	return gulp.src('./dist', {
+gulp.task("clean", function () {
+	return gulp.src("./dist", {
 		read: false
 	}).pipe(clean());
 });
 
-gulp.task('restart-nwjs', function () {
+gulp.task("restart-nwjs", function () {
 	if (nwjs) {
 		let kill_command;
-		if (process.platform == 'win32') {
-			kill_command = 'taskkill /F /IM nw.exe';
+		if (process.platform == "win32") {
+			kill_command = "taskkill /F /IM nw.exe";
 		} else {
-			kill_command = 'killall -KILL nw';
+			kill_command = "killall -KILL nw";
 		}
 		child_process.exec(kill_command, function (err) {
 			if (err != null && !err.killed)
 				console.error(err);
 		});
 	}
-	nwjs = child_process.exec('nw .', function (err) {
+	nwjs = child_process.exec("nw .", function (err) {
 		if (err != null && !err.killed)
 			console.error(err);
 	});
-	return gulp.src('./');
+	return gulp.src("./");
 });
 
-gulp.task('build-info-message', function () {
-	console.log('Execute the following command to build the project nwbuild . -p win32,win64,osx32,osx64,linux32,linux64 --flavor normal');
+gulp.task("build-info-message", function () {
+	console.log("Execute the following command to build the project nwbuild . -p win32,win64,osx32,osx64,linux32,linux64 --flavor normal");
 	console.log(`The project will be build in ${process.cwd()}/build`);
-	return gulp.src('./');
+	return gulp.src("./");
 });
 
-gulp.task('default', gulp.series('clean', gulp.parallel('copy-html', 'styles', 'js-lint', 'scripts', 'minify-images'), 'restart-nwjs', 'watch'));
-gulp.task('export', gulp.series('clean', gulp.parallel('copy-html', 'styles', 'js-lint', 'scripts-dist', 'minify-images'), 'js-tests', 'build-info-message'));
+gulp.task("default", gulp.series("clean", gulp.parallel("copy-html", "styles", "js-lint", "scripts", "minify-images"), "restart-nwjs", "watch"));
+gulp.task("export", gulp.series("clean", gulp.parallel("copy-html", "styles", "js-lint", "scripts-dist", "minify-images"), "js-tests", "build-info-message"));
