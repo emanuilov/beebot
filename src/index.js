846d936c70c1be581f8c5f1bdcc168dbc91c3c8e
Radostin Emanuilov <emanuilov@techr.eu>
2019-07-08 19:52:30 +0100

    Renaming

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { useRoutes } from 'hookrouter';
-import routes from './Router';
+import routes from './routes';
 import * as serviceWorker from './serviceWorker';
 import './sass/main.scss';
 
